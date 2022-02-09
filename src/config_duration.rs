b877a8f7819b42478365a2ab80f3bc15a0a144d0
Josh Klar <josh@klar.sh>
2022-02-08 16:06:45 -0800

    feat: begin lua-side stdlib, implement combinators & validators

--- src/config_duration.rs
+++ src/config_duration.rs
@@ -8,30 +8,27 @@ pub enum ConfigDuration {
     Seconds(u64),
 }
 
-impl From<ConfigDuration> for Duration {
-    fn from(src: ConfigDuration) -> Self {
-        (&src).into()
-    }
-}
-
 impl From<&ConfigDuration> for Duration {
     fn from(src: &ConfigDuration) -> Self {
         match src {
-            ConfigDuration::Milliseconds(ms) => Duration::from_millis(*ms),
-            ConfigDuration::Seconds(ms) => Duration::from_secs(*ms),
+            ConfigDuration::Milliseconds(ms) => Self::from_millis(*ms),
+            ConfigDuration::Seconds(ms) => Self::from_secs(*ms),
         }
     }
 }
 
 #[test]
 fn test_seconds() {
-    assert_eq!(Duration::from_secs(10), ConfigDuration::Seconds(10).into());
+    assert_eq!(
+        Duration::from_secs(10),
+        (&ConfigDuration::Seconds(10)).into()
+    );
 }
 
 #[test]
 fn test_milliseconds() {
     assert_eq!(
         Duration::from_millis(100),
-        ConfigDuration::Milliseconds(100).into()
+        (&ConfigDuration::Milliseconds(100)).into()
     );
 }
--- src/step_lua.rs
+++ src/step_lua.rs
@@ -1,10 +1,9 @@
 use rlua::{Lua, RegistryKey};
 
-use std::rc::Rc;
-
 use crate::pipe_contents::PipeContents;
 use crate::pipeline::StepCompletion;
-use crate::step_error::StepError;
+use crate::shared_lua::run_user_script_function;
+use crate::step_error::StepResult;
 
 pub fn step_function<'a>(
     idx: usize,
@@ -15,24 +14,14 @@ pub fn step_function<'a>(
     user_script_registry_key: &'a RegistryKey,
 
     last: Option<&'a PipeContents>,
-) -> Result<StepCompletion, StepError> {
-    lua.context(|ctx| {
-        let lua_func = ctx
-            .registry_value::<rlua::Table>(user_script_registry_key)?
-            .get::<_, rlua::Function>(fname)?;
-        let script_arg = match last {
-            Some(lval) => match lval.to_lua(lua)? {
-                Some(rkey) => ctx.registry_value::<rlua::Value>(&rkey)?,
-                None => rlua::Nil,
-            },
-            None => rlua::Nil,
-        };
-        let result = lua_func.call::<rlua::Value, rlua::Value>(script_arg)?;
-        let registry_key = ctx.create_registry_value(result)?;
-
-        Ok(StepCompletion::Normal {
-            next_index: idx + 1,
-            pipe_data: Some(PipeContents::LuaReference(Rc::new(registry_key))),
-        })
+) -> StepResult {
+    Ok(StepCompletion::Normal {
+        next_index: idx + 1,
+        pipe_data: Some(PipeContents::LuaReference(run_user_script_function(
+            fname,
+            lua,
+            user_script_registry_key,
+            last,
+        )?)),
     })
 }
