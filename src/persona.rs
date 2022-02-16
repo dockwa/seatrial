053f41314c77f5ebdb3a88c5f9d71cea151d98f8
Josh Klar <josh@klar.sh>
2022-02-16 13:50:54 -0800

    chore: simplify personas to be an inline trait of a grunt, rather than a reference to a map

--- src/persona.rs
+++ src/persona.rs
@@ -5,12 +5,24 @@ use std::collections::HashMap;
 use crate::config_duration::ConfigDuration;
 use crate::pipeline::action::{ConfigActionMap, PipelineAction};
 
-// built out of a PersonaSpec during Situation construction
 #[derive(Clone, Debug)]
 pub struct Persona {
-    pub name: String,
-    pub spec: PersonaSpec,
+    pub timeout: ConfigDuration,
     pub headers: HashMap<String, String>,
+    pub sequence: Vec<PipelineAction>,
+}
+
+impl From<&PersonaSpec> for Persona {
+    fn from(spec: &PersonaSpec) -> Self {
+        Self {
+            timeout: spec.timeout.clone(),
+            sequence: spec.sequence.clone(),
+
+            // TODO: populate with Value/LuaFunction returns, error on other PipelineAction
+            // variants
+            headers: HashMap::new(),
+        }
+    }
 }
 
 #[derive(Clone, Debug, DeRon)]
