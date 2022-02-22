94c9dbdba2feb0fad3fa493656d95cf303951efc
paul <paul@dockwa.com>
2022-02-22 13:49:20 -0500

    chore(lint): Address some rustfmt feedback

--- examples/simpleish/server.rs
+++ examples/simpleish/server.rs
@@ -1,6 +1,6 @@
 use chrono::prelude::*;
-use vial::prelude::*;
 use nanoserde::{DeJson, DeJsonErr};
+use vial::prelude::*;
 
 use std::error::Error;
 use std::fmt::{Display, Formatter};
@@ -84,10 +84,7 @@ fn create_profile(req: Request) -> Result<String, CreateProfileError> {
 
     let profile: Profile = DeJson::deserialize_json(req.body())?;
 
-    let response = format!(
-        "Successfully created profile for {}.",
-        profile.email
-    );
+    let response = format!("Successfully created profile for {}.", profile.email);
 
     eprintln!("debug: {}", response);
 
